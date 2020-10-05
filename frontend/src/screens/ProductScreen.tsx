b174182103013052cf0b1377211dab42ee8af903
Jakub Figlak <jakub.figlak@gmail.com>
2020-10-05 20:27:30 +0200

    Create ProductScreen component, add global readme to list all the changes compared to original project

--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -1,5 +1,85 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Rating } from '../components/Rating';
+import products from '../products';
+
+type ParamTypes = {
+  id: string;
+};
 
 export const ProductScreen = () => {
-  return <div>Product</div>;
+  const { id } = useParams<ParamTypes>();
+
+  const product = products.find((p) => p._id === id);
+
+  if (!product) {
+    return <p>No product found with the id of {id}</p>;
+  }
+
+  const {
+    brand,
+    category,
+    name,
+    image,
+    description,
+    price,
+    numReviews,
+    countInStock,
+    rating,
+  } = product;
+
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={image} alt={name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={rating} text={`${numReviews} reviews`} />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${price}</ListGroup.Item>
+            <ListGroup.Item>Description: {description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>{countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
